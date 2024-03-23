e1355e57dd02a1a2517e4a7950da38c07f7fb6e4
赌狗 <2737550037@qq.com>
2024-03-24 00:36:24 +0800

    init: 🎉 项目初始化

---
+++ src/main/java/com/dragon/dragondriver/DragonDriverApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.dragondriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonDriverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dragon/dragondriver/DragonDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.dragondriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonDriverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
